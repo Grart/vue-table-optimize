ddf0b3ca7c31bd7e9a1b9ad43ecc4bedc225b857
grart <grart_01@msn.com>
2019-07-26 11:59:35 +0800

    1

--- src/example/ComponentTest.vue
+++ src/example/ComponentTest.vue
@@ -10,14 +10,17 @@
         </cmp1>-->
         <cmp2>
         </cmp2>
+        <button @click="onCreate">create</button>
+
+        <button @click="onDestroy">destroy</button>
     </div>
 </template>
 
 <script lang="ts">
+    import Vue from 'vue'
 
     const cmp1 = {
-        data()
-        {
+        data() {
             return {
                 val: 1
             }
@@ -25,16 +28,14 @@
         props: {
             name: String
         },
-        render: function (h, params)
-        {
+        render: function (h, params) {
             let _$this = this;
-            console.log('render cmp1',_$this);
+            console.log('render cmp1', _$this);
             return h(
                 "div",
                 {
                     on: {
-                        click: function (e)
-                        {
+                        click: function (e) {
                             _$this.val++;
                         }
                     }
@@ -42,22 +43,18 @@
                 `${_$this.name}:${_$this.val}`
             );
         },
-        mounted: function ()
-        {
+        mounted: function () {
             console.log('mounted cmp1');
         }
     };
 
-
     const cmp2 = {
-        data()
-        {
+        data() {
             return {
                 val: 1
             }
         },
-        render: function (h, params)
-        {
+        render: function (h, params) {
             let _$this = this;
             console.log('render cmp2');
             return h(
@@ -69,13 +66,12 @@
                         "button",
                         {
                             on: {
-                                click: function (e)
-                                {
+                                click: function (e) {
                                     _$this.val++;
                                 }
                             }
                         },
-                        "Cmp2 - "+_$this.val
+                        "Cmp2 - " + _$this.val
                     ),
                     h(
                         cmp1,
@@ -104,25 +100,60 @@
                 ]
             );
         },
-        mounted: function ()
-        {
+        mounted: function () {
             console.log('mounted cmp2');
         }
     };
+
+
+    var _cmpCtor = Vue.extend(cmp1);
+    let _cmpInstance = null;
+    let _el: HTMLElement = null;
+
     export default {
         name: 'VirtualScrollTable',
-        components: {cmp1, cmp2},
-        data()
-        {
+        components: { cmp1, cmp2 },
+        data() {
             return {
             };
         },
         methods: {
+            onCreate: function () {
+                if (null == _el) {
+                    _el = document.createElement('div');
+                    document.body.append(_el);
+                }
+                if (null == _cmpInstance) {
+                    _cmpInstance = new _cmpCtor();
+                    _cmpInstance.$mount();
+                    console.log(_cmpInstance.elm);
+                }
+
+            },
+            onDestroy: function () {
+
+                if (null != _cmpInstance) {
+                    let _el = _cmpInstance.$el;
+                    if (_el.remove) {
+                        console.log('remove');
+                        _el.remove();
+                    }
+                    else if (_el.parentNode && _el.parentNode.removeChild) {
+                        console.log('removeChild');
+                        _el.parentNode.removeChild(_el);
+                    }
+                    _el.remove();
+                    _el = null;
+                    _cmpInstance.$destroy();
+                    _cmpInstance = null;
+                }
+            }
         }
     };
 </script>
 
 <style>
+
     .header-title__virtual {
         background-color: #f8f8f9;
         color: #495060;
